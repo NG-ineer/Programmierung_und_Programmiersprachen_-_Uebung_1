c0ad47701e4ae014e27440253ecf8618ee69f691
NG-ineer <92338026+NG-ineer@users.noreply.github.com>
2023-05-12 18:06:31 +0200

    added calculation for surface area of a circle

--- src/MeinProgramm.java
+++ src/MeinProgramm.java
@@ -1,8 +1,13 @@
 public class MeinProgramm{
     public static void main(String[] args){
         System.out.println("Hallo Welt!");
+
         double radius=4;
+
         double umfang=2*Math.PI*radius;
+        double flaeche=2*Math.PI*radius*radius/4;
+
         System.out.println("Umfang vom Kreis mit Radius = "+radius+" ist gleich "+umfang);
+        System.out.println("Flächen vom Kreis mit Radius = "+radius+" ist gleich "+flaeche);
     }
 }
