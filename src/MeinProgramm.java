850967ad636daa8a975149d8e31c882f57dfcf99
NG-ineer <92338026+NG-ineer@users.noreply.github.com>
2023-05-12 17:58:24 +0200

    added calculation for circumference of a circle

--- src/MeinProgramm.java
+++ src/MeinProgramm.java
@@ -1,5 +1,8 @@
 public class MeinProgramm{
     public static void main(String[] args){
         System.out.println("Hallo Welt!");
+        double radius=4;
+        double umfang=2*Math.PI*radius;
+        System.out.println("Umfang vom Kreis mit Radius = "+radius+" ist gleich "+umfang);
     }
 }
